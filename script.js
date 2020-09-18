93099700f192a063e78ff29d13842470f9e895ac
Estefania <nialupo87@gmail.com>
2020-09-18 10:51:36 -0400

    added generatePassword to resolve error

--- script.js
+++ script.js
@@ -10,5 +10,9 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  return "A string for the password";
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
