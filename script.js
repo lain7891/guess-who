00ea3063ce2d73568f5a766980b7ac3c2a0127de
Estefania <nialupo87@gmail.com>
2020-09-18 21:46:48 -0400

    added function for lowercase and upper case letters and number

--- script.js
+++ script.js
@@ -14,5 +14,21 @@ function generatePassword(){
   return "This will be replaced by me generated password.";
 }
 
+var userInput = prompt("How many characters to generate your password? You can choose between 8 to 128"); 
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+console.log(getRandomLower());
+console.log(getRandomUpper());
+console.log(getRandomNumber());
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
