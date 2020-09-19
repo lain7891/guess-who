372f6814586fed17ff76b3eed42bdafd83405d61
Estefania <nialupo87@gmail.com>
2020-09-18 21:58:34 -0400

    added symbols functions and consoled log

--- script.js
+++ script.js
@@ -27,8 +27,14 @@ function getRandomUpper() {
 function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
+
+function getRandomSymbol() {
+  symbols = '~`!@#$%^&*()_+-={}|:"<>?,./;'; 
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
 console.log(getRandomLower());
 console.log(getRandomUpper());
 console.log(getRandomNumber());
+console.log(getRandomSymbol());
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
